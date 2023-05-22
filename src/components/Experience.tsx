fd0643c3261d2de30f96cf219c11c143e2c68496
sh1ggy <tyronewessnolasco@gmail.com>
2023-05-22 23:14:30 +1000

    Dynamic bio & mapping

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,24 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+export interface Bio {
+  jobName: string;
+  bio: string[];
+}
 
 export default function Experience() {
-  const BIO = [
-    {
-      jobName: "Sonic IT",
-      bio:
-        [
-          "Responsible for engaging with internal Sonic IT teams & external vendors via thorough communication to assist with incident resolution and management of critical incidents.",
-          "Maintenance of case-by-case incident documentation and team-wide knowledge articles as a part of a collaborative effort to document the support process for 30+ different pieces of software.",
-        ]
-    },
-    {
-      jobName: "Wings Education",
-      bio:
-        [
-          "Placeholder",
-          "Placeholder",
-        ]
-    },
-  ]
+  const BIO =
+    [
+      {
+        jobName: "Sonic IT",
+        bio:
+          [
+            "Responsible for engaging with internal Sonic IT teams & external vendors via thorough communication to assist with incident resolution and management of critical incidents.",
+            "Maintenance of case-by-case incident documentation and team-wide knowledge articles as a part of a collaborative effort to document the support process for 30+ different pieces of software.",
+          ]
+      },
+      {
+        jobName: "Wings Education",
+        bio:
+          [
+            "Managed and developed PHP-based Learning Management System website using Moodle & SQL that aimed to increase staff and student productivity.",
+            "Mentored cohorts of 2 - 3 IT interns every 4 months in basic IT troubleshooting, social media design and LMS maintenance skills in addition to teaching essential communication skills.",
+          ]
+      },
+    ]
   const [selectedJob, setSelectedJob] = useState<string>(BIO[0].jobName);
   return (
     <div className="bg-card flex flex-col space-y-6 rounded-2xl p-10 h-full justify-center mx-36">
@@ -30,7 +36,7 @@ export default function Experience() {
         </div>
 
         <div className="flex flex-col">
-          <div className="bg-primary text-right p-2 rounded-t-lg">{selectedJob}</div>
+          <div className="bg-primary text-right p-2 pr-6 rounded-t-lg">{selectedJob}</div>
           <div className="flex flex-row bg-[#838383] rounded-b-lg">
             <div className="flex flex-col rounded-br-lg text-body">
               {BIO
@@ -39,8 +45,9 @@ export default function Experience() {
                 .map(({ b, i }) => {
                   return (
                     <button
+                      key={i}
                       onClick={() => setSelectedJob(b.jobName)}
-                      className="bg-page px-24 break-keep">{b.jobName}</button>
+                      className="bg-page pr-24 pl-6 py-3 whitespace-nowrap text-left w-30">{b.jobName}</button>
                   )
                 })
               }
@@ -48,25 +55,12 @@ export default function Experience() {
             <div className="bg-container rounded-br-lg">
               <ul className="list-disc pl-12 p-8">
                 {BIO
-                  .map((b, i) => ({ b, i }))
-                  .filter(({ b, i }) => b.jobName == selectedJob)
-                  .map(({ b, i }) => {
-                    return (
-                      <li className="text-body">{b.bio[i]}</li>
-                    )
-                  })
+                  .map((b) => (b))
+                  .filter((b) => b.jobName == selectedJob)
+                  .map((b) => b.bio)
+                  .map((d) => d)[0]
+                  .map((d, i) => (<li key={i} className="text-body">{d}</li>))
                 }
-                {/* <li className="text-body">
-                  Responsible for engaging with internal Sonic IT teams &
-                  external vendors via thorough communication to assist with
-                  incident resolution and management of critical incidents.
-                </li>
-                <li className="text-body">
-                  Maintenance of case-by-case incident documentation and
-                  team-wide knowledge articles as a part of a collaborative
-                  effort to document the support process for 30+ different
-                  pieces of software.
-                </li> */}
               </ul>
             </div>
           </div>
