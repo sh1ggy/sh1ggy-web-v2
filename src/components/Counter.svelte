04487920d36979ccc8b60d6b671cfbb8f683e4f0
Leon Ahuja <leon.ahuja@tandm.ai>
2023-08-22 05:58:53 +1000

    more comment

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -3,6 +3,7 @@
   function handleClick() {
     counter++;
   }
+  // Conditionally render either the bpm modes using astro
 </script>
 <div>{counter}</div>
 <button on:click={handleClick}>click</button>
