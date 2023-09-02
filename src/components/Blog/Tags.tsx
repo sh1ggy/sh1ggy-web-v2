713d7a2d18b66e5311f383297f341693be4c3a13
sh1ggy <tyronewessnolasco@gmail.com>
2023-09-02 16:54:06 +1000

    NaNoRenO blog post & tag error handling

--- src/components/Blog/Tags.tsx
+++ src/components/Blog/Tags.tsx
@@ -4,9 +4,9 @@ interface Props {
 export default function Tags({ tags }: Props) {
   return (
     <div className="flex flex-row flex-wrap gap-3 rounded-lg mt-3">
-      {
-        tags.map((tag: string) => (
-          <div className="bg-card rounded-lg p-2">{tag}</div>
+      { tags && 
+        tags.map((tag: string, i: number) => (
+          <div key={i} className="bg-card rounded-lg p-2">{tag}</div>
         ))
       }
     </div>
